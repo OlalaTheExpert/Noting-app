6b38bf2423aca4cf7c63b874b5e6b4270c0ad8c9
sonykarl <sonykarl14@gmail.com>
2021-06-24 11:00:31 +0300

    activity changes

--- app/src/main/java/com/example/noted/MainActivity.kt
+++ app/src/main/java/com/example/noted/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.noted
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val switchpage2btn: Button = findViewById(R.id.addnotesbtn)
+        switchpage2btn.setOnClickListener{
+            val intention = Intent(this, addnotespage::class.java)
+            startActivity(intention)
+        }
+
+
+
     }
 }
